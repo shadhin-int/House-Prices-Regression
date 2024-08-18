9a938c55e80e1de70329f88c22a6532db16fe65e
shadhin-int <shadhin.int@gmail.com>
2024-08-18 22:44:45 +0600

    write test case for health check and model generation

--- test/test_text_classification.py
+++ test/test_text_classification.py
@@ -10,6 +10,20 @@ def sample_request_data():
     return {"text": "This is a test document."}
 
 
+def test_health_check():
+    response = client.get("/")
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data == {"status": "API is running smoothly"}
+
+
+def test_generate_model():
+    response = client.get("/text_classification/generate-model")
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data == {"message": "Model generated successfully"}
+
+
 def test_classify_text(sample_request_data):
     response = client.post("/text_classification/classify/", json=sample_request_data)
     assert response.status_code == 200
